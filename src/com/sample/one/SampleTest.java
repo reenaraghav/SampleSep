1d0cfafa88601017db40aa31b17dc621dd7b98a2
reenaraghav <42831518+reenaraghav@users.noreply.github.com>
2018-09-15 18:49:10 +0530

    Update SampleTest.java

--- src/com/sample/one/SampleTest.java
+++ src/com/sample/one/SampleTest.java
@@ -5,7 +5,7 @@ public class SampleTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome");
-		
+		System.out.println("Reena");
 		System.out.println("changes----------");
 	}
 
